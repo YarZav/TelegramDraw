9033940ab0225d59ff57ef9ce938032a94daeff0
Iaroslav Zavialov <>
2022-10-11 20:51:43 +0200

    Added initial project

---
+++ TelegramDraw/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TelegramDraw
+//
+//  Created by 19205313 on 08.03.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+}
